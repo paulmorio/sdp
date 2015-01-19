172bc88b9725e7f9024f1736ed575e7f10d55b0f
Chris Seaton <chrst.sea@gmail.com>
2015-01-19 23:01:15 +0000

    Updated arduino code with SerialCommand library - will link up with python communicator class

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,21 +1,43 @@
+#include <SerialCommand.h>
+
 /*
-  Simple arduino code to accompany initial java communicator.
-  
-  This sketch parses an integer from serial then returns its successor,
-  merely a ping test.
+  Arduino code for SDP Group 7 2014
   
   Author: Chris Seaton
 */
 
+// Communications
+SerialCommand comm;
+
 void setup() {
-  digitalWrite(8, HIGH);  // enable RF
-  Serial.begin(115200);  // default baudrate for SRF
+  // Set RF pin and select radio
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
+  // Set LED pin
+  pinMode(13, OUTPUT);
+  
+  // Init serial at default SRF baudrate
+  Serial.begin(115200);
+  
+  comm.addCommand("A_KICK", kick);
+  comm.addCommand("A_PUNCH", punch);
+  comm.setDefaultHandler(invalid_command);
+  
   Serial.println("<Arduino is ready>");
 }
 
 void loop() {
-  while (Serial.available() > 0) {
-    int in = Serial.parseInt();
-    Serial.write(in);  // write input back to serial - for testing
-  }
+  comm.readSerial();
 }
+
+void kick() {
+  digitalWrite(13, HIGH);
+  Serial.println("Kicked!");
+}
+
+void punch() {
+  digitalWrite(13, LOW);
+  Serial.println("Punched!");
+}
+
+void invalid_command(const char* command) {}
