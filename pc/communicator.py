821812777e33ce4bbd292eb95ac0cd04853de542
Chris Seaton <chrst.sea@gmail.com>
2015-01-20 20:50:01 +0000

    Refactored the Communicator class after realising that, for all of its lengthiness, it does little more than the Serial class itself. The Communicator class now implements a simple read_all() method to read all data on the serial line, and sets different defaults for the init.

--- pc/communicator.py
+++ pc/communicator.py
@@ -1,7 +1,7 @@
-from serial import *
+from serial import Serial
 
 
-class Communicator:
+class Communicator(Serial):
     """
     Initialise serial connection to robot. Handles communication back and forth.
     """
@@ -11,7 +11,7 @@ class Communicator:
         Create a communicator which acts as an interface for serial
         communication.
 
-        :param port: Default is "/dev/ttyUSB0". This changes based on
+        :param port: Default is "/dev/ttyACM0". This changes based on
         platform, etc. The default should correspond to what shows on the DICE
         machines.
 
@@ -19,50 +19,7 @@ class Communicator:
         :param timeout: write timeout in seconds
         :return:
         """
-
-        self.serial = None
-        self.port = port
-        self.rate = rate
-        self.timeout = timeout
-        self.setup()
-
-    def setup(self):
-        """ Initialise the serial port. Print an error if already open or if
-            failed. """
-        if self.serial is None:
-            try:
-                self.serial = Serial(self.port, self.rate, timeout=self.timeout)
-            except SerialException:
-                print "Unable to open serial connection. It is likely that " \
-                      "the port is incorrect or the device is missing."
-        else:
-            print "A serial port is already open - go away!"
-
-    def write(self, str):
-        """
-        Write a string to the serial device.
-        TODO writing ints - need to encode int to C byte representation
-
-        :param str: output string
-        :return:
-        """
-        if self.serial:
-            self.serial.write(str)
-        else:
-            print "Serial is not initialized"
-
-    def read(self, size=1):
-        """
-        Read n bytes from serial.
-
-        :param size: number of bytes read from serial
-        :return: data read from serial.
-        """
-        if self.serial:
-            data = self.serial.read(size=size)
-            return data
-        else:
-            print "Serial is not initialized"
+        super(Communicator, self).__init__(port, rate, timeout=timeout)
 
     def read_all(self):
         """
@@ -70,16 +27,4 @@ class Communicator:
 
         :return: Byte representation of data on serial.
         """
-        if self.serial:
-            return self.read(size=self.serial.inWaiting())
-        else:
-            print "Serial is not initialized"
-
-    def close(self):
-        """
-        Clean up and close serial port.
-
-        :return:
-        """
-        self.serial.close()
-        self.serial = None
+        return self.read(self.inWaiting())
