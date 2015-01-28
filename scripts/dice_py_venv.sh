6a8b8e0c1c0e193825131af63693aed6f3d161cb
Chris Seaton <s1241352@ed.ac.uk>
2015-01-28 21:00:03 +0000

    Added python 2.7 virtual environment creation script for DICE

---
+++ scripts/dice_py_venv.sh
@@ -0,0 +1,12 @@
+ # This script will create a Python 2.7 virtual environment
+ # with dependencies (see requirements.txt) in the project 
+ # root directory.
+ # Note that this is for use on UoE DICE machines only - I 
+ # am making several assumptions based on this.
+ # If you are met with 'Permission denied' then you should
+ # set permissions with chmod - i.e `chmod 777 this_script.sh`
+
+ virtualenv --distribute --python=/usr/bin/python2.7 ../venv
+ source ../venv/bin/activate
+ pip install -r requirements.txt
+ deactivate
