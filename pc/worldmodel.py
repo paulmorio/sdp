98eab3bacd3e38599d71cf68a0542c0e63f7226f
Paul Scherer <s1206798@sms.ed.ac.uk>
2015-01-30 22:03:58 +0000

    Added robots and vectors to be part of the world

--- pc/worldmodel.py
+++ pc/worldmodel.py
@@ -4,7 +4,31 @@ Module Describes the state of the world which the football robots reside in.
 A world state can be maintained and updated via instantiation of the Worldmodel.
 All of the other classes inside of this module model entities that are attributes
 of the world state.
+
+TODO: Write all the getters setters and discuss some of the parameters for acceptability with the others
+
 """
+
+class Vector():
+	"""
+	This class helps describe some of the attributes to be used by the other
+	objects on the field.
+
+	"""
+
+	def __init__(self, x_pos, y_pos, angle, velocity):
+		"""
+		
+		:param x_pos: Describes the x position of the (x,y) locational coordinate
+		:param y_pos: Describes the y position of the (x,y) locational coordinate
+		:param angle: The angle of the vector (ie, where the vector is facing)
+		:param velocity: The magnitude of the vector
+		"""
+
+		self.x_pos = x_pos
+		self.y_pos = y_pos
+		self.angle = angle
+		self.velocity = velocity
 
 class Ball():
 
@@ -94,13 +118,19 @@ class Robot():
 	"""
 	This class is different from the robot class defined in the robot module
 	Objects of this class at this stage only contain information of a robot's 
-	position and orientation described by a (x,y) coordinate and vector 
-	respectively
+	position and orientation described by a vector 
 	"""
 
-	
+	def __init__(self, x_pos, y_pos, angle, velocity):
+		"""
 
+		:param vector: The vector parameter is of type Vector as described in
+		the helping class of the same name. It describes the position of the robot
+		through the (x,y) coordinates, the orientation of the robot through angle, 
+		and the velocity in which the robot is moving in. 
+		"""
 
+		self.vector = vector(x_pos, y_pos, angle, velocity)
 
 
 
