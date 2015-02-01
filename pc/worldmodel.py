0716f2bc426e536432da1a4b81a0311d932bc045
Paul Scherer <s1206798@bugatti.inf.ed.ac.uk>
2015-02-01 03:03:40 +0000

    Completed the worldmodel in theory, need to discuss with team about implementing and further developing this as it is incomplete in practice.

--- pc/worldmodel.py
+++ pc/worldmodel.py
@@ -5,8 +5,8 @@ A world state can be maintained and updated via instantiation of the Worldmodel.
 All of the other classes inside of this module model entities that are attributes
 of the world state.
 
-TODO: Write all the getters setters and discuss some of the parameters for acceptability with the others
-
+TODO: Write all the getters setters and discuss some of the parameters for 
+acceptability with the others
 """
 
 class Vector():
@@ -30,7 +30,28 @@ class Vector():
 		self.angle = angle
 		self.velocity = velocity
 
+	def getX_pos():
+		return self.x_pos
+
+	def getY_pos():
+		return self.y_pos
+
+	def getAngle():
+		return self.angle
+
+	def getVelocity():
+		return self.velocity
+
+	def update(new_x, new_y, new_angle, new_velocity):
+		self.x_pos = new_x
+		self.y_pos = new_y
+		self.angle = new_angle
+		self.velocity = new_velocity
+
 class Ball():
+	"""
+	The Ball is defined simply as a (x,y) coordinate
+	"""
 
 	def __init__(self, x_pos, y_pos):
 		"""
@@ -42,6 +63,11 @@ class Ball():
 
 		self.x_pos = x_pos
 		self.y_pos = y_pos
+
+	def update(new_x, new_y):
+		self.x_pos = new_x
+		self.y_pos = new_y
+
 
 class DefensiveField():
 	"""
@@ -80,6 +106,15 @@ class DefensiveField():
 		self.cord5 = cord5
 		self.cord6 = cord6
 
+	def update(newCord1, newCord2, newCord3, newCord4, newCord5, newCord6):
+		self.cord1 = newCord1
+		self.cord2 = newCord2
+		self.cord3 = newCord3
+		self.cord4 = newCord4
+		self.cord5 = newCord5
+		self.cord6 = newCord6
+
+
 class OffensiveField():
 	"""
 	This class defines an offensive area of the field through 4 cordinate tuples (x,y)
@@ -114,6 +149,13 @@ class OffensiveField():
 		self.cord3 = cord3
 		self.cord4 = cord4
 
+	def update(newCord1, newCord2, newCord3, newCord4):
+		self.cord1 = newCord1
+		self.cord2 = newCord2
+		self.cord3 = newCord3
+		self.cord4 = newCord4
+
+
 class Robot():
 	"""
 	This class is different from the robot class defined in the robot module
@@ -132,9 +174,48 @@ class Robot():
 
 		self.vector = vector(x_pos, y_pos, angle, velocity)
 
+	def update(new_x, new_y, new_angle, new_velocity):
+		self.vector.update(new_x, new_y, new_angle, new_velocity)
 
 
+class WorldState():
+	"""
+	worldstate objects contain information about the state of the world (the 
+	football pitch and its inhabitants) 
+	"""
 
-class Worldmodel():
+	def __init__(self, pitch, odf, tdf, oof, tof, odrobot, tdrobot, oorobot, torobot, ball):
+		"""
+		The standard way to initialize a worldstate, with all the inhabitants
 
-    
+		:param pitch: The rectangular pitch as a whole
+		:param odf: Our defensive field
+		:param tdf: Their defensive field
+		:param oof: Our Offensive field
+		:param tof: Their offensive field
+		:param odrobot: Our defensive robot
+		:param tdrobot: Their defensive robot
+		:param oorobot: Our offensive robot
+		:param torobot: their offensive robot
+		:param ball: The Ball (as a ball object of this module)
+		:return: 
+		"""
+
+		# replace "stuff" with parameters after discussion with team.
+		self.pitch = Pitch(stuff)
+		self.odf = DefensiveField(stuff)
+		self.tdf = DefensiveField(stuff)
+		self.oof = OffensiveField(stuff)
+		self.tof = OffensiveField(stuff)
+		self.odrobot = Robot(stuff)
+		self.tdrobot = Robot(stuff)
+		self.oorobot = Robot(stuff)
+		self.torobot = Robot(stuff)
+		self.ball = Ball(stuff)
+
+	def update(new_pitch, new_odf, new_tdf, new_oof, new_tof, new_odrobot, new_tdrobot, 
+		new_oorobot, new_torobot, new_ball):
+		# have to discuss how to implement this.
+		pass
+
+
