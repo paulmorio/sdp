fa7c1f82ec86106b205fb9b94bb579322e8232b9
Chris Seaton <s1241352@ed.ac.uk>
2015-02-02 13:06:40 +0000

    Fixed a bug (line 50 of cl2cpp.cmake halts installation) - should work now

--- scripts/opencv_setup.sh
+++ scripts/opencv_setup.sh
@@ -1,12 +1,15 @@
 # Install OpenCV 2.4.10 on DICE vision PCs
 cd /disk/scratch/sdp
+rm -rf opencv-2.4.10*
 wget sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.10/opencv-2.4.10.zip
 unzip opencv-2.4.10.zip
 cd opencv-2.4.10
 mkdir build
 cd build
 cmake -D WITH_OPENCL=OFF -D WITH_CUDA=OFF -D BUILD_opencv_gpu=OFF -D BUILD_opencv_gpuarithm=OFF -D BUILD_opencv_gpubgsegm=OFF -D BUILD_opencv_gpucodec=OFF -D BUILD_opencv_gpufeatures2d=OFF -D BUILD_opencv_gpufilters=OFF -D BUILD_opencv_gpuimgproc=OFF -D BUILD_opencv_gpulegacy=OFF -D BUILD_opencv_gpuoptflow=OFF -D BUILD_opencv_gpustereo=OFF -D BUILD_opencv_gpuwarping=OFF -D CMAKE_INSTALL_PREFIX=~/.local ..
+sed -i '50d' ../cmake/cl2cpp.cmake
 make
 make install
 echo 'export PYTHONPATH=$PYTHONPATH:~/.local/lib/python2.6/site-packages' >> ~/.brc
 cd /disk/scratch/sdp
+rm -rf opencv-2.4.10*
