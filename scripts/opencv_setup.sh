41100c96bdd393cb8d5a0a63faf1b7a002d2756c
Murray <1232200@sms.ed.ac.uk>
2015-02-04 14:23:49 +0000

    Updated venv script to use Python2.6, removed old OpenCV install code, added conditional (hopefully) to OpenCV install script.

--- scripts/opencv_setup.sh
+++ scripts/opencv_setup.sh
@@ -1,18 +1,28 @@
 # Install OpenCV 2.4.10 on DICE vision PCs
-mkdir /disk/scratch/sdp/group7
-cd /disk/scratch/sdp/group7
-rm -rf opencv-2.4.10*
-wget sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.10/opencv-2.4.10.zip
-unzip opencv-2.4.10.zip
-cd opencv-2.4.10
-mkdir build
-cd build
-cmake -D WITH_OPENCL=OFF -D WITH_CUDA=OFF -D BUILD_opencv_gpu=OFF -D BUILD_opencv_gpuarithm=OFF -D BUILD_opencv_gpubgsegm=OFF -D BUILD_opencv_gpucodec=OFF -D BUILD_opencv_gpufeatures2d=OFF -D BUILD_opencv_gpufilters=OFF -D BUILD_opencv_gpuimgproc=OFF -D BUILD_opencv_gpulegacy=OFF -D BUILD_opencv_gpuoptflow=OFF -D BUILD_opencv_gpustereo=OFF -D BUILD_opencv_gpuwarping=OFF -D CMAKE_INSTALL_PREFIX=~/.local ..
-sed -i '50d' ../cmake/cl2cpp.cmake
-make
-make install
-# This kills the .brc -- TODO add to path only if necessary
-echo 'export PYTHONPATH=$PYTHONPATH:~/.local/lib/python2.6/site-packages' >> ~/.brc
-cd /disk/scratch/sdp/group7
-rm -rf opencv-2.4.10.zip
-cd
+
+# If Group 7 has already had their sticky paws all over this PC, skip all this
+if ! [ -d "/disk/scratch/sdp/group7" ]; then
+    mkdir /disk/scratch/sdp/group7
+    cd /disk/scratch/sdp/group7
+    rm -rf opencv-2.4.10*
+    wget sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.10/opencv-2.4.10.zip
+    unzip opencv-2.4.10.zip
+    cd opencv-2.4.10
+    mkdir build
+    cd build
+    cmake -D WITH_OPENCL=OFF -D WITH_CUDA=OFF -D BUILD_opencv_gpu=OFF -D BUILD_opencv_gpuarithm=OFF -D BUILD_opencv_gpubgsegm=OFF -D BUILD_opencv_gpucodec=OFF -D BUILD_opencv_gpufeatures2d=OFF -D BUILD_opencv_gpufilters=OFF -D BUILD_opencv_gpuimgproc=OFF -D BUILD_opencv_gpulegacy=OFF -D BUILD_opencv_gpuoptflow=OFF -D BUILD_opencv_gpustereo=OFF -D BUILD_opencv_gpuwarping=OFF -D CMAKE_INSTALL_PREFIX=~/.local ..
+    sed -i '50d' ../cmake/cl2cpp.cmake
+    make
+    make install
+    # This kills the .brc -- TODO add to path only if necessary
+    echo 'export PYTHONPATH=$PYTHONPATH:~/.local/lib/python2.6/site-packages' >> ~/.brc
+    cd /disk/scratch/sdp/group7
+    rm -rf opencv-2.4.10.zip
+    cd
+
+# And instead just install OpenCV and add to Python path
+else
+    cd /disk/scratch/sdp/group7/opencv-2.4.10/build
+    make install
+    echo 'export PYTHONPATH=$PYTHONPATH:~/.local/lib/python2.6/site-packages' >> ~/.brc
+fi
--- scripts/py_venv_setup.sh
+++ scripts/py_venv_setup.sh
@@ -6,13 +6,7 @@
  # If you are met with 'Permission denied' then you should
  # set permissions with chmod - i.e `chmod 777 this_script.sh`
 
- virtualenv --distribute --python=/usr/bin/python2.7 ../venv
+ virtualenv --distribute --python=/usr/bin/python2.6 ../venv
  source ../venv/bin/activate
  pip install -r requirements.txt
  deactivate
-
- # This should "install" OpenCV, give that it can't be pip'd
- # The "clean" way to do this:
- # http://stackoverflow.com/questions/11184847/running-opencv-from-a-python-virtualenv/24112175#24112175
- # This appears to require building OpenCV, though, which takes a very long time.
- # cp ../OpenCV/* ../venv/lib/python2.7/site-packages/
