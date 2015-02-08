2c0f8f4e698872d11b3cd4ff130ab0b0f73f02c8
Paul Scherer <s1206798@sms.ed.ac.uk>
2015-02-08 03:40:16 +0000

    Fixed most of the functions to accept current parameters and actually make the robot move, renamed a few functions as well so that they better describe what is happening, as well as the accompanying comments

--- pc/planner.py
+++ pc/planner.py
@@ -1,6 +1,8 @@
 from math import pi, sqrt
 from worldmodel import *
 
+# Methods that 
+
 class Planner():
 
     # MILESTONE
@@ -50,11 +52,11 @@ class Planner():
         elif direction == 'none':
             self.robotController.command(Robot.STOP)
         else:
-            print "ERROR in get_rotation_direction"
+            print "ERROR in get_direction_to_rotate"
 
     def bot_rotate_or_move(self, direction):
         """
-        Quick 'n' dirty - rotates bot towards given direction, or moves forward if the direction is none
+        Rotates bot towards given direction, or moves forward if the direction is none
         """
         if direction == 'right':
             self.robotController.command(Robot.ROTATE_RIGHT)
@@ -63,44 +65,46 @@ class Planner():
         elif direction == 'none':
             self.robotController.command(Robot.MOVE_FORWARD)
         else:
-            print "ERROR in get_rotation_direction"
+            print "ERROR in get_direction_to_rotate"
 
-    # MILESTONE
     def bot_get_ball(self):
         """
-        Quick 'n' dirty - has the bot move to the ball, and grab it
+        Has the bot move to the ball, and grab it
         """
-        ball = self.world.ball
+        ball = self.world._ball
 
-        if not self.bot.can_catch_ball(ball):  # If the ball is not within catching range, move towards it
-            # If we need to rotate to face the ball, do so, otherwise just move to it
-            dir_to_rotate = self.get_rotation_direction(ball)
-            self.bot_rotate_and_go(dir_to_rotate)
+        # We have to make sure we dont have the ball already
+        if not self.bot.has_ball:
+                    # If the robot cannot catch the ball yet
+            if not self.bot.can_catch_ball(ball):
+                dir_to_rotate = self.get_direction_to_rotate(ball)
+                self.bot_rotate_and_go(dir_to_rotate)
 
-        else:  # Otherwise (if the ball is within catching range) "grab" until we have possession of the ball
-            if not self.bot.has_ball(ball):
-                self.bot.command(self.bot.GRAB)
+            # Otherwise (if the ball is within catching range) "grab" until we have possession of the ball
+            else:
+                self.robotController.command(Robot.GRAB)
 
-    # MILESTONE
-    def bot_own_goal(self):
+        else:
+            pass
+
+    def rotate_towards_goal_and_shoot(self):
         """
-        Quick 'n' dirty - when the bot has possession of the ball, will cause it to rotate towards own goal and shoot
+        when the bot has possession of the ball, will cause it to rotate to their goal and shoot
         """
         # Now that we have the ball, rotate towards the own goal.
-        dir_to_rotate = self.get_rotation_direction(self.world.our_goal)
-        if dir_to_rotate != 'none':  # If we need to rotate, do so
+        dir_to_rotate = self.get_direction_to_rotate(self.world.their_goal)
+        if dir_to_rotate != 'none':
             self.bot_rotate(dir_to_rotate)
         else:  # Finished rotating to face goal, can now kick
-            self.bot.command(self.bot.KICK)
-            #self.bot.command(PREMATURE_CELEBRATION)
+            self.robotController.command(Robot.KICK)
 
     # MILESTONE
     def bot_pass_forward(self):
         """
-        Quick 'n' dirty - when the bot has possession of the ball, will cause it to rotate towards their goal and pass
+        When the bot has possession of the ball, will cause it to rotate towards their goal and pass
         """
         # Now that we have the ball, rotate towards the right attacking zone.  # IMPORTANT: 'side' has to be 'left'
-        dir_to_rotate = self.get_rotation_direction(self.world.their_goal)
+        dir_to_rotate = self.get_direction_to_rotate(self.world.their_goal)
         if dir_to_rotate != 'none':  # If we need to rotate, do so
             self.bot_rotate(dir_to_rotate)
         else:  # Finished rotating to face goal, can now kick
@@ -131,7 +135,7 @@ class Planner():
             elif ball.y < self.bot.y - 5:
                 self.bot.command(self.bot.MOVE_BACK)
 
-    # MILESTONE
+    # DEPRECATED?
     def attacker(self):
         """
         Have the robot move to and grab a stationary ball, then rotate to face a goal and kick it
@@ -146,7 +150,7 @@ class Planner():
         else:
             self.bot.own_goal()
 
-    # MILESTONE
+    # DEPRECATED?
     def defender(self):
         """
         Have the robot stay on one axis until the ball is in its zone
@@ -163,7 +167,7 @@ class Planner():
             self.bot_pass_forward()
 
 
-    # MILESTONE
+    # DEPRECATED?
     def chase(self):
         """
         Simple function to have robot go to ball (regardless of competition constraints)
@@ -190,7 +194,7 @@ class Planner():
 
     #     if displacement > 20:  # 20 here is completely arbitrary, it should be a "safe" distance at which the robot can stop in front of the ball
     #         # Rotate-first
-    #         dir_to_rotate = self.get_rotation_direction(ball)
+    #         dir_to_rotate = self.get_direction_to_rotate(ball)
     #         self.bot_rotate_and_go(dir_to_rotate)
 
     #         """
@@ -240,26 +244,26 @@ class Planner():
                 pass
 
 
-        # # Find the different situations (states) the defender can be in
-        # elif self.mode == 'defender':
+        # Find the different situations (states) the defender can be in
+        elif self.mode == 'defender':
 
-        #     if (self.state == 'noBall'):
-        #         if (not ball_is_owned()):
-        #             if (not is_grabber_opened):
-        #                 open_grabbers()
-        #             self.robotController.move_vertical(pitch_get_height()/2)
-        #         else:
-        #             if (not ball_is_idle()):
-        #                 self.robotController.move_vertical(ball._y)
-        #                 if (not aiming_towards_object(ball)):
-        #                      self.robotController.rotate_towards_point(ball._x,ball._y)
-        #                 distance_to_ball = self.bot.get_displacement_to_point(ball._x,ball._y)
-        #                 if (distance_to_ball == 0): # TODO update 0 into variable depending on future: ball velocity, attempt to close grabbers exaclty at the time the ball rolls into grabbers
-        #                     if (is_grabber_opened()):
-        #                         self.robotController.close_grabbers()
-        #                     self.state = 'hasBall'
-        #             else:
-        #                 self.mode = 'Dog' # FETCH!! (WARNING: doggie style does not care about our field in the pitch)
+            if (self.state == 'noBall'):
+                if (not ball_is_owned()):
+                    if (not is_grabber_opened):
+                        open_grabbers()
+                    self.robotController.move_vertical(pitch_get_height()/2)
+                else:
+                    if (not ball_is_idle()):
+                        self.robotController.move_vertical(ball._y)
+                        if (not aiming_towards_object(ball)):
+                             self.robotController.rotate_towards_point(ball._x,ball._y)
+                        distance_to_ball = self.bot.get_displacement_to_point(ball._x,ball._y)
+                        if (distance_to_ball == 0): # TODO update 0 into variable depending on future: ball velocity, attempt to close grabbers exaclty at the time the ball rolls into grabbers
+                            if (is_grabber_opened()):
+                                self.robotController.close_grabbers()
+                            self.state = 'hasBall'
+                    else:
+                        self.mode = 'Dog' # FETCH!! (WARNING: doggie style does not care about our field in the pitch)
 
         # # Dog mode
         # elif self.mode == 'Dog':
@@ -313,7 +317,7 @@ class Planner():
             angle_to_turn_to = self.bot.get_rotation_to_point(ball_x,ball_y)
             distance_to_move = self.bot.get_displacement_to_point(ball_x, ball_y)
 
-            dir_to_turn = get_rotation_direction(self.world._ball)
+            dir_to_turn = get_direction_to_rotate(self.world._ball)
 
             # We command the robot turn to the ball, and move forward if it is facing it.
             # This is implementation is deeply simplified (but works)
