b998ec8e6fbc47e784b0ce064f9d2078c7027787
Chris Seaton <s1241352@ed.ac.uk>
2015-03-12 13:06:08 +0000

    Pretty much done - still need an effective way to terminate the subprocess

--- pc/communicator.py
+++ pc/communicator.py
@@ -3,7 +3,6 @@ from serial import Serial
 ACK_LEN = 8  # Length of ack string received from robot
 CMD_DELIMITER = ' '
 CMD_TERMINAL = '\n'
-EXIT_CODE = "EXIT"
 
 class Communicator(object):
     """
@@ -38,17 +37,17 @@ class Communicator(object):
         while True:
             if self.current_command is None:  # No current command, get
                 cmd = self.comm_pipe.recv()
-                if cmd == EXIT_CODE:
-                    break
-                else:
-                    self.current_command = cmd + CMD_DELIMITER + \
-                                           self.ack_bit + CMD_TERMINAL
+                self.current_command = cmd + CMD_DELIMITER + \
+                                       self.ack_bit + CMD_TERMINAL
 
             else:  # Have a current command, get ack/state
                 self.serial.write(self.current_command)
                 ack = self.serial.readline()
                 if self.ack_test_and_update(ack):
                     self.current_command = None  # Successful ack, we're done
+
+        # Exit sequence
+        self.comm_pipe.write(EXIT_CODE)
 
     def ack_test_and_update(self, ack):
         """
